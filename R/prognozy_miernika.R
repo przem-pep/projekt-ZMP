bd23e2bb2a0dd783a7d0782ea2a19e03f48efb23
przem-pep <przpeplinski@gmail.com>
2025-11-11 14:55:38 +0100

    dużo rzeczy

--- R/prognozy_miernika.R
+++ R/prognozy_miernika.R
@@ -1,19 +1,252 @@
 library(tidyverse)
 library(forecast)
+library(tseries)
+library(gt)
+
+
+# Deklaracja funkcji
+
+# Normalizacja min-max
+minmax <- function(x, type = "stymulanta") {
+  min_val = min(x)
+  max_val = max(x)
+  if(type == "stymulanta") {
+    return((x - min_val) / (max_val - min_val))
+  } else if(type == "destymulanta") {
+    return((max_val - x) / (max_val - min_val))
+  }
+}
+
+# Błędy prognoz wygasłych
+metrics <- function(actual, pred) {
+  me = mean(pred - actual)
+  mpe = 100 * mean((pred - actual) / actual)
+  mae = mean(abs(pred - actual))
+  mape = 100 * mean(abs((pred - actual) / actual))
+  rmse = sqrt(mean((pred - actual) ^ 2))
+  rmspe = 100 * sqrt(mean(((pred - actual) / actual) ^ 2))
+  result = data.frame(ME = me, MPE = mpe,
+                      MAE = mae, MAPE = mape,
+                      RMSE = rmse, RMSPE = rmspe)
+  return(result)
+}
+
+# Optymalizowanie modeli ARIMA i SARIMA za pomocą RMSPE
+create_arima_models <- function(x, actual) {
+  parametre_range <- 0:2
+  n <- length(parametre_range) ^ 4 - 1
+  result <- tibble(p = rep(NA, n),
+                   q = rep(NA, n),
+                   P = rep(NA, n),
+                   Q = rep(NA, n),
+                   RMSPE = rep(NA, n))
+  i <- 0
+  cat("Rozpoczynam", n, "iteracji\n")
+  for(p in parametre_range) {
+    for(q in parametre_range) {
+      for(P in parametre_range) {
+        for(Q in parametre_range) {
+          if(p + q + P + Q == 0) {
+            next
+          }
+          i <- i + 1
+          current_model <- try(Arima(x, order = c(p, 0, q), seasonal = c(P, 0, Q)), silent = TRUE)
+          if(inherits(current_model, "try-error")) {
+            result[i, "p"] <- p
+            result[i, "q"] <- q
+            result[i, "P"] <- P
+            result[i, "Q"] <- Q
+            cat("Iteracja", i, "nie powiodła się.\n")
+            next
+          } else {
+            current_forecast <- forecast(current_model, h = 24)
+            current_metrics <- metrics(actual, current_forecast$mean)
+            result[i, "p"] <- p
+            result[i, "q"] <- q
+            result[i, "P"] <- P
+            result[i, "Q"] <- Q
+            result[i, "RMSPE"] <- current_metrics[, "RMSPE"]
+            cat("Iteracja", i, "powiodła się.\n")
+          }
+        }
+      }
+    }
+  }
+  return(result)
+}
+
+
+# Wczytanie danych
 
 dane <- read_csv("data/dane.csv")
+dane_bez_dat <- dane %>%
+  select(-date)
+
 
 # Standaryzacja
 
-dane_scale <- dane %>%
-  select(-date) %>%
+dane_std <- dane_bez_dat %>%
   sapply(scale) %>%
   as_tibble()
 
-dane_scale$miernik <- rowSums(dane_scale)
 
-dane_scale$date <- dane$date
+# Budowanie wskaźnika EWM
 
-train <- slice_head(dane_scale, n = 108)
-test <- slice_tail(dane_scale, n = 24)
+zmienne <- list(hdd = "destymulanta",
+                import_gazu = "stymulanta",
+                import_ropy = "stymulanta",
+                zuzycie_energii = "destymulanta",
+                zuzycie_gazu = "destymulanta",
+                dostawy_prod_naft = "stymulanta",
+                dostawy_wegla = "stymulanta",
+                cena_akcji_orlen = "stymulanta",
+                cena_akcji_energa = "stymulanta",
+                cena_gazu = "destymulanta",
+                cena_ropy = "destymulanta")
+
+variables <- names(zmienne)
+
+dane_unit <- dane_bez_dat %>%
+  mutate(
+    across(
+      .cols = all_of(names(zmienne)),
+      .fns = ~ minmax(., type = zmienne[[cur_column()]])
+      )
+    )
+
+sumy_kolumn <- colSums(dane_bez_dat)
+p_ij <- sweep(dane_bez_dat, 2, sumy_kolumn, FUN = "/")
+p_ij[p_ij == 0] <- 1e-10
+E_j <- - (1 / log(nrow(dane_bez_dat))) * colSums(p_ij * log(p_ij))
+d_j <- 1 - E_j
+w_j <- d_j / sum(d_j)
+names(w_j) <- colnames(dane_bez_dat)
+
+dane_ewm <- dane_unit
+
+for(i in 1:ncol(dane_unit)) {
+  dane_ewm[, i] <- dane_unit[, i] * w_j[i]
+}
+
+indeks_ewm <- rowSums(dane_ewm)
+
+
+# Analiza głównych składowych
+
+pca_result <- dane_std %>%
+  select(all_of(variables)) %>%
+  prcomp(center = FALSE, scale. = FALSE)
+
+
+# Stworzenie mierników
+
+dane_std <- dane_std %>%
+  mutate(miernik_ew = rowMeans(pick(all_of(variables))),
+         miernik_pca = pca_result$x[, 1],
+         miernik_ewm = indeks_ewm,
+         date = dane$date)
+
+
+# Train test split
+
+train <- slice_head(dane_std, n = 108)
+test <- slice_tail(dane_std, n = 24)
+
+
+# Mierniki jako szeregi czasowe
+
+miernik_ew <- ts(train$miernik_ew, start = c(2014, 1), frequency = 12)
+miernik_pca <- ts(train$miernik_pca, start = c(2014, 1), frequency = 12)
+miernik_ewm <- ts(train$miernik_ewm, start = c(2014, 1), frequency = 12)
+
+
+# Testy Ljung-Boxa dla mierników
+
+Box.test(miernik_ew, type = "Ljung-Box")
+Box.test(miernik_pca, type = "Ljung-Box")
+Box.test(miernik_ewm, type = "Ljung-Box")
+
+
+# Testy stacjonarności dla mierników
+
+adf.test(miernik_ew)
+adf.test(miernik_pca)
+adf.test(miernik_ewm)
+
+
+# Budowanie prognoz metodą Holta-Wintersa
+
+fc_ew_hw <- forecast(HoltWinters(miernik_ew), h = 24)
+fc_pca_hw <- forecast(HoltWinters(miernik_pca), h = 24)
+fc_ewm_hw <- forecast(HoltWinters(miernik_ewm), h = 24)
+
+
+# Znalezienie najlepszego modelu z modeli ARIMA i SARIMA na podstawie RMSPE
+
+models_ew <- create_arima_models(miernik_ew, test$miernik_ew)
+models_pca <- create_arima_models(miernik_pca, test$miernik_pca)
+models_ewm <- create_arima_models(miernik_ewm, test$miernik_ewm)
+
+
+# Najlepsze modele ARIMA/SARIMA dla każdego miernika
+
+models_ew %>%
+  slice_min(RMSPE)
+
+models_pca %>%
+  slice_min(RMSPE)
+
+models_ewm %>%
+  slice_min(RMSPE)
+
+
+# Zbudowanie prognoz dla najlepszych modeli ARIMA/SARIMA
+
+fc_ew_arima <- forecast(Arima(miernik_ew, order = c(0, 0, 1), seasonal = c(0, 0, 0)), h = 24) # 001 000 / 001 004
+fc_pca_arima <- forecast(Arima(miernik_pca, order = c(0, 0, 2), seasonal = c(2, 0, 0)), h = 24) # 002 200 / 001 204
+fc_ewm_arima <- forecast(Arima(miernik_ewm, order = c(0, 0, 2), seasonal = c(1, 0, 1)), h = 24) # 002 101 / 004 101
+
+
+# Błędy prognoz dla wszystkich metod
+
+met_ew_hw <- metrics(test$miernik_ew, fc_ew_hw$mean)
+met_ew_arima <- metrics(test$miernik_ew, fc_ew_arima$mean)
+
+met_pca_hw <- metrics(test$miernik_pca, fc_pca_hw$mean)
+met_pca_arima <- metrics(test$miernik_pca, fc_pca_arima$mean)
+
+met_ewm_hw <- metrics(test$miernik_ewm, fc_ewm_hw$mean)
+met_ewm_arima <- metrics(test$miernik_ewm, fc_ewm_arima$mean)
+
+
+# Podsumowanie prognoz
+
+podsumowanie_prognoz <- bind_rows(met_ew_hw, met_ew_arima,
+                                  met_pca_hw, met_pca_arima,
+                                  met_ewm_hw, met_ewm_arima) %>%
+  mutate(metoda = c("EW Holt Winters", "EW ARIMA",
+                    "PCA Holt Winters", "PCA ARIMA",
+                    "EWM Holt Winters", "EWM ARIMA")) %>%
+  select(metoda, everything())
+
+
+# Dane do wizualizacji
+
+prognozy <- tibble(
+  date = test$date,
+  prognoza_ew_hw = fc_ew_hw$mean,
+  prognoza_ew_arima = fc_ew_arima$mean,
+  prognoza_pca_hw = fc_pca_hw$mean,
+  prognoza_pca_arima = fc_pca_arima$mean,
+  prognoza_ewm_hw = fc_ewm_hw$mean,
+  prognoza_ewm_arima = fc_ewm_arima$mean
+)
+
+dane_wizualizacje_bezposrednie <- dane_std %>%
+  select(date, miernik_ew, miernik_pca, miernik_ewm) %>%
+  full_join(prognozy, by = "date") %>%
+  pivot_longer(cols = c(-date))
+
+
+write_csv(dane_wizualizacje_bezposrednie, file = "data/dane_wizualizacje_bezposrednie.csv")
 
---
+++ R/wizualizacje.R
@@ -0,0 +1,255 @@
+library(tidyverse)
+
+dane_wizualizacje_zmiennych <- read_csv("data/dane.csv") %>%
+  pivot_longer(cols = c(-date))
+dane_wizualizacje_bezposrednie <- read_csv("data/dane_wizualizacje_bezposrednie.csv")
+
+
+# Wykresy pierwszych pięciu zmiennych
+
+wykres_zmiennych_1 <- dane_wizualizacje_zmiennych %>%
+  filter(name %in% c("hdd", "import_gazu", "import_ropy", "zuzycie_energii", "zuzycie_gazu")) %>%
+  ggplot(aes(x = date, y = value)) +
+  geom_line(linewidth = 0.75, colour = "grey15") +
+  facet_wrap(~ name, scales = "free", ncol = 2,
+             labeller = labeller(
+               name = c("hdd" = "Liczba stopniodni grzania (hdd)",
+                        "import_gazu" = "Import gazu w mln m^3 (import_gazu)",
+                        "import_ropy" = "Import ropy w tys. ton (import_ropy)",
+                        "zuzycie_energii" = "Zużycie energii elektrycznej w GWh (zuzycie_energii)",
+                        "zuzycie_gazu" = "Zużycie gazu w mln m^3 (zuzycie_gazu)"))) +
+  scale_x_date(date_breaks = "2 years",
+               date_labels = "%Y") +
+  labs(x = NULL, y = NULL) +
+  theme_minimal() +
+  theme(
+    strip.text = element_text(size = 11, face = "bold"),
+    axis.text = element_text(size = 10),
+    plot.background = element_rect(fill = "white", colour = "black"))
+
+ggsave("figures/wykres_zmiennych_1.png", wykres_zmiennych_1)
+
+
+# Wykresy pozostałych sześciu zmiennych
+
+wykres_zmiennych_2 <- dane_wizualizacje_zmiennych %>%
+  filter(name %in% c("dostawy_prod_naft", "dostawy_wegla", "cena_akcji_orlen", "cena_akcji_energa", "cena_gazu", "cena_ropy")) %>%
+  ggplot(aes(x = date, y = value)) +
+  geom_line(linewidth = 0.75, colour = "grey15") +
+  facet_wrap(~ name, scales = "free", ncol = 2,
+             labeller = labeller(
+               name = c("dostawy_prod_naft" = "Dostawy produktów naftowych\nw tys. ton (dostawy_prod_naft)",
+                        "dostawy_wegla" = "Dostawy węgla kamiennego w tys. ton (dostawy_wegla)",
+                        "cena_akcji_orlen" = "Cena akcji ORLEN w zł (cena_akcji_orlen)",
+                        "cena_akcji_energa" = "Cena akcji ENERGA SA w zł (cena_akcji_energa)",
+                        "cena_gazu" = "Cena gazu w USD za mln brytyjskich\njednostek cieplnych (cena_gazu)",
+                        "cena_ropy" = "Cena ropy w USD za baryłkę (cena_ropy)"))) +
+  scale_x_date(date_breaks = "2 years",
+               date_labels = "%Y") +
+  labs(x = NULL, y = NULL) +
+  theme_minimal() +
+  theme(
+    strip.text = element_text(size = 11, face = "bold"),
+    axis.text = element_text(size = 10),
+    plot.background = element_rect(fill = "white", colour = "black"))
+
+ggsave("figures/wykres_zmiennych_2.png", wykres_zmiennych_2)
+
+
+# Prognoza bezpośrednia indeksu EW (Holt_winters)
+
+wykres_ew_hw_bezp <- dane_wizualizacje_bezposrednie %>%
+  filter(name %in% c("miernik_ew", "prognoza_ew_hw")) %>%
+  ggplot(aes(x = date, y = value, colour = name, linetype = name)) +
+  geom_line(linewidth = 1.1) +
+  scale_colour_manual(
+    name = NULL,
+    values = c("miernik_ew" = "grey15",
+               "prognoza_ew_hw" = "blue"),
+    labels = c("Obserwacje", "Prognoza")) +
+  scale_linetype_manual(
+    name = NULL,
+    values = c("miernik_ew" = "solid",
+               "prognoza_ew_hw" = "dotted"),
+    labels = c("Obserwacje", "Prognoza")
+  ) +
+  scale_x_date(date_breaks = "1 year",
+               date_labels = "%Y") +
+  labs(title = "Prognoza bezpośrednia indeksu EW (Holt-Winters)",
+       x = NULL,
+       y = NULL) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16),
+    axis.text = element_text(size = 11),
+    legend.text = element_text(size = 11),
+    legend.position = "bottom",
+    plot.background = element_rect(fill = "white", colour = "black"))
+
+ggsave("figures/wykres_ew_hw_bezp.png", wykres_ew_hw_bezp)
+
+
+# Prognoza bezpośrednia indeksu EW (ARIMA)
+
+wykres_ew_arima_bezp <- dane_wizualizacje_bezposrednie %>%
+  filter(name %in% c("miernik_ew", "prognoza_ew_arima")) %>%
+  ggplot(aes(x = date, y = value, colour = name, linetype = name)) +
+  geom_line(linewidth = 1.1) +
+  scale_colour_manual(
+    name = NULL,
+    values = c("miernik_ew" = "grey15",
+               "prognoza_ew_arima" = "blue"),
+    labels = c("Obserwacje", "Prognoza")) +
+  scale_linetype_manual(
+    name = NULL,
+    values = c("miernik_ew" = "solid",
+               "prognoza_ew_arima" = "dotted"),
+    labels = c("Obserwacje", "Prognoza")
+  ) +
+  scale_x_date(date_breaks = "1 year",
+               date_labels = "%Y") +
+  labs(title = "Prognoza bezpośrednia indeksu EW (ARIMA)",
+       x = NULL,
+       y = NULL) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16),
+    axis.text = element_text(size = 11),
+    legend.text = element_text(size = 11),
+    legend.position = "bottom",
+    plot.background = element_rect(fill = "white", colour = "black"))
+
+ggsave("figures/wykres_ew_arima_bezp.png", wykres_ew_arima_bezp)
+
+
+# Prognoza bezpośrednia indeksu PCA (Holt-Winters)
+
+wykres_pca_hw_bezp <- dane_wizualizacje_bezposrednie %>%
+  filter(name %in% c("miernik_pca", "prognoza_pca_hw")) %>%
+  ggplot(aes(x = date, y = value, colour = name, linetype = name)) +
+  geom_line(linewidth = 1.1) +
+  scale_colour_manual(
+    name = NULL,
+    values = c("miernik_pca" = "grey15",
+               "prognoza_pca_hw" = "blue"),
+    labels = c("Obserwacje", "Prognoza")) +
+  scale_linetype_manual(
+    name = NULL,
+    values = c("miernik_pca" = "solid",
+               "prognoza_pca_hw" = "dotted"),
+    labels = c("Obserwacje", "Prognoza")
+  ) +
+  scale_x_date(date_breaks = "1 year",
+               date_labels = "%Y") +
+  labs(title = "Prognoza bezpośrednia indeksu PCA (Holt-Winters)",
+       x = NULL,
+       y = NULL) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16),
+    axis.text = element_text(size = 11),
+    legend.text = element_text(size = 11),
+    legend.position = "bottom",
+    plot.background = element_rect(fill = "white", colour = "black"))
+
+ggsave("figures/wykres_pca_hw_bezp.png", wykres_pca_hw_bezp)
+
+
+# Prognoza bezpośrednia indeksu PCA (ARIMA)
+
+wykres_pca_arima_bezp <- dane_wizualizacje_bezposrednie %>%
+  filter(name %in% c("miernik_pca", "prognoza_pca_arima")) %>%
+  ggplot(aes(x = date, y = value, colour = name, linetype = name)) +
+  geom_line(linewidth = 1.1) +
+  scale_colour_manual(
+    name = NULL,
+    values = c("miernik_pca" = "grey15",
+               "prognoza_pca_arima" = "blue"),
+    labels = c("Obserwacje", "Prognoza")) +
+  scale_linetype_manual(
+    name = NULL,
+    values = c("miernik_pca" = "solid",
+               "prognoza_pca_arima" = "dotted"),
+    labels = c("Obserwacje", "Prognoza")
+  ) +
+  scale_x_date(date_breaks = "1 year",
+               date_labels = "%Y") +
+  labs(title = "Prognoza bezpośrednia indeksu PCA (ARIMA)",
+       x = NULL,
+       y = NULL) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16),
+    axis.text = element_text(size = 11),
+    legend.text = element_text(size = 11),
+    legend.position = "bottom",
+    plot.background = element_rect(fill = "white", colour = "black"))
+
+ggsave("figures/wykres_pca_arima_bezp.png", wykres_pca_arima_bezp)
+
+
+# Prognoza bezpośrednia indeksu EWM (Holt-Winters)
+
+wykres_ewm_hw_bezp <- dane_wizualizacje_bezposrednie %>%
+  filter(name %in% c("miernik_ewm", "prognoza_ewm_hw")) %>%
+  ggplot(aes(x = date, y = value, colour = name, linetype = name)) +
+  geom_line(linewidth = 1.1) +
+  scale_colour_manual(
+    name = NULL,
+    values = c("miernik_ewm" = "grey15",
+               "prognoza_ewm_hw" = "blue"),
+    labels = c("Obserwacje", "Prognoza")) +
+  scale_linetype_manual(
+    name = NULL,
+    values = c("miernik_ewm" = "solid",
+               "prognoza_ewm_hw" = "dotted"),
+    labels = c("Obserwacje", "Prognoza")
+  ) +
+  scale_x_date(date_breaks = "1 year",
+               date_labels = "%Y") +
+  labs(title = "Prognoza bezpośrednia indeksu EWM (Holt-Winters)",
+       x = NULL,
+       y = NULL) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16),
+    axis.text = element_text(size = 11),
+    legend.text = element_text(size = 11),
+    legend.position = "bottom",
+    plot.background = element_rect(fill = "white", colour = "black"))
+
+ggsave("figures/wykres_ewm_hw_bezp.png", wykres_ewm_hw_bezp)
+
+
+# Prognoza bezpośrednia indeksu EWM (ARIMA)
+
+wykres_ewm_arima_bezp <- dane_wizualizacje_bezposrednie %>%
+  filter(name %in% c("miernik_ewm", "prognoza_ewm_arima")) %>%
+  ggplot(aes(x = date, y = value, colour = name, linetype = name)) +
+  geom_line(linewidth = 1.1) +
+  scale_colour_manual(
+    name = NULL,
+    values = c("miernik_ewm" = "grey15",
+               "prognoza_ewm_arima" = "blue"),
+    labels = c("Obserwacje", "Prognoza")) +
+  scale_linetype_manual(
+    name = NULL,
+    values = c("miernik_ewm" = "solid",
+               "prognoza_ewm_arima" = "dotted"),
+    labels = c("Obserwacje", "Prognoza")
+  ) +
+  scale_x_date(date_breaks = "1 year",
+               date_labels = "%Y") +
+  labs(title = "Prognoza bezpośrednia indeksu EWM (ARIMA)",
+       x = NULL,
+       y = NULL) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 16),
+    axis.text = element_text(size = 11),
+    legend.text = element_text(size = 11),
+    legend.position = "bottom",
+    plot.background = element_rect(fill = "white", colour = "black"))
+
+ggsave("figures/wykres_ewm_arima_bezp.png", wykres_ewm_arima_bezp)
+
